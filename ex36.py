246864b2161352e54545a8c9435d3f5a9750bd08
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-09-22 00:36:41 -0300

    Update ex36.py

--- ex36.py
+++ ex36.py
@@ -1,13 +1,27 @@
 numero = int(input('digite um número: '))
-n = 0
-while n <= 2:
-    if n == 0:
-        print('converção para binario')
-        per = str(input('sim / não :')).lower()
-        if per in 'sim s':
-            print('deu certo')
-            n = 2
-    if n == 1:
-        print('converção para octal')
-        per = str(input('sim / não')).lower()
-    n += 1
+def conversão(a):
+    n = 0
+    while n <= 2:
+        if n == 0:
+            print('converção para binario')
+            per = str(input('sim / não :')).lower()
+            if per in 'sim s':
+                print('deu certo')
+                n = 2
+                return 'b'
+        if n == 1:
+            print('converção para octal')
+            per = str(input('sim / não ')).lower()
+            if per in 'sim s':
+                n = 2
+                return 'o'
+        if n == 2:
+            print('converção para hexadecimal :')
+            per = str(input('sim / não' )).lower
+            if per in 'sim s':
+                print('deu certo 3')
+                
+                
+            
+resultado = conversão(numero)
+print(resultado)
