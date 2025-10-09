052830ba4bd295c0e6076eadad00bb8c937c3b97
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-09 13:15:21 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -8,17 +8,18 @@ janela.title('Exercício 02')
 janela.state('zoomed')
 # texto da tarefa 
 texto = tk.Label(janela, text='Exercício 02- perguntas sobre data de nascimento', font=('Arial',15))
-texto.place(x=800, y=50)
+texto.place(x=400, y=50)
 def movi():
     y = randint(40, 60)
-    texto.place(x=800, y = y) 
-    janela.after(1000, movi)
+    texto.place(x=700, y = y) 
+    janela.after(200, movi)
 movi() 
 
 # fazer os botoes para salvar 
-while True:
-    pergunta 
-
+for c in range(0, 2):
+    list = ['dia', 'mês', 'ano']
+    pergunta = tk.Label(janela, text='Qual o {} do seu nascimento?'.format(list[c]), font=('Arial',12))
+    pergunta.place(x=800, y=150)
 janela.mainloop()
 
 # Exercício 02 - Crie um script Python que leia o dia, o mês e o
