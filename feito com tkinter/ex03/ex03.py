dfe5515f356fdc77a81c079e6c909512046dc61e
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-11 11:17:47 -0300

    iniciei p terceiro exercício

--- feito com tkinter/ex03/ex03.py
+++ feito com tkinter/ex03/ex03.py
@@ -7,6 +7,9 @@ janela = tk.Tk()
 janela.title('Exercício 03')
 janela.geometry('800x500')
 janela.resizable(False, False)
-
+#icone da janela 
+janela.iconbitmap('icone.ico')
+texto_ex = tk.Label(janela, text='Exercício 03 ', font=('ArialBlack', 12))
+texto_ex.place(x=350, y=30)
 
 janela.mainloop()
