b775acdbe7daaabf2b7a8b9ae629da080766d12c
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-11 10:38:36 -0300

    .exe exercício 02
    fiz um .exe do prinmeiro exercício usando o pyinstaller --onefile

--- ex03.py
+++
@@ -1,3 +0,0 @@
-n1 = int(input('digite um valor: '))
-n2 = int(input('digite ouytro valor: '))
-print('a soma entre {} e {} e {}'.format(n1, n2, n1+n2))
---
+++ feito com tkinter/ex03/ex03.py
@@ -0,0 +1,12 @@
+##n1 = int(input('digite um valor: '))
+##n2 = int(input('digite ouytro valor: '))
+#print('a soma entre {} e {} e {}'.format(n1, n2, n1+n2)) esse é o exercicio 
+import tkinter as tk 
+
+janela = tk.Tk()
+janela.title('Exercício 03')
+janela.geometry('800x500')
+janela.resizable(False, False)
+
+
+janela.mainloop()
