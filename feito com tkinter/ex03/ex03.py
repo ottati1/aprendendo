9d3ce2445893d8b80af0a3a4f7079652e03ba69c
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-11 13:24:06 -0300

    Update ex03.py

--- feito com tkinter/ex03/ex03.py
+++ feito com tkinter/ex03/ex03.py
@@ -2,14 +2,22 @@
 ##n2 = int(input('digite ouytro valor: '))
 #print('a soma entre {} e {} e {}'.format(n1, n2, n1+n2)) esse é o exercicio 
 import tkinter as tk 
+import random as rd
 
 janela = tk.Tk()
 janela.title('Exercício 03')
-janela.geometry('800x500')
+janela.geometry('400x500')
 janela.resizable(False, False)
 #icone da janela 
 janela.iconbitmap('icone.ico')
 texto_ex = tk.Label(janela, text='Exercício 03 ', font=('ArialBlack', 12))
-texto_ex.place(x=350, y=30)
+texto_ex.place(x=150, y=30)
+def movi():
+    x = rd.randint(100, 200)
+    y = rd.randint(15, 45)
+    texto_ex.place(x=x, y=y)
+    texto_ex.after(300), movi)
+movi()
+
 
 janela.mainloop()
