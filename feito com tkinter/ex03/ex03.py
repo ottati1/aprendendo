1acbde349674c3cedf98434a8d09e669780c6365
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-11 13:50:01 -0300

    Update ex03.py

--- feito com tkinter/ex03/ex03.py
+++ feito com tkinter/ex03/ex03.py
@@ -8,16 +8,21 @@ janela = tk.Tk()
 janela.title('Exercício 03')
 janela.geometry('400x500')
 janela.resizable(False, False)
+janela.configure(bg='#FF7F7F')
 #icone da janela 
 janela.iconbitmap('icone.ico')
-texto_ex = tk.Label(janela, text='Exercício 03 ', font=('ArialBlack', 12))
+texto_ex = tk.Label(janela, text='Exercício 03 ', font=('ArialBlack', 12), bg='#FF7F7F')
 texto_ex.place(x=150, y=30)
 def movi():
     x = rd.randint(100, 200)
     y = rd.randint(15, 45)
-    texto_ex.place(x=x, y=y)
-    texto_ex.after(300), movi)
+    texto_ex.place(x=150, y=y)
+    texto_ex.after(300, movi)
 movi()
+janela_resposta = tk.Entry(janela)
+janela_resposta.place(x= 135, y=90)
 
 
+
+janela_resposta.focus()
 janela.mainloop()
