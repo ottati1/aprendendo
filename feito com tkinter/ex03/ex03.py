7d86cd88a9f10b3773648f079427e8b166d63732
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-11 23:47:01 -0300

    Update ex03.py

--- feito com tkinter/ex03/ex03.py
+++ feito com tkinter/ex03/ex03.py
@@ -19,16 +19,32 @@ def movi():
     texto_ex.place(x=150, y=y)
     texto_ex.after(300, movi)
 movi()
+
 janela_resposta = tk.Entry(janela)
 janela_resposta.place(x= 135, y=90)
+
+#texto da pergunra
+pergunta_txt = tk.Label(janela, text='Digite o primeiro valor', font=('ArialBlack', 12), bg="#ff7f7f")
+pergunta_txt.place(x=119 , y=70)
 #botão salvar com as funçoes 
+
 def salvar():
     valor = janela_resposta.get()
     lista_calculo.append(valor)
-    print(lista_calculo)
-
-
-
+    print(lista_calculo, len(lista_calculo))
+    c = 0
+    if len(lista_calculo) >= 1:
+        if len(lista_calculo) <= 1:
+            pergunta_txt.config(text='Digite o segundo valor')
+        if len(lista_calculo) >=2:
+            pergunta_txt.destroy()
+            botao.destroy()
+            janela_resposta.destroy()
+            valor = tk.Label(janela, text=lista_calculo[0], font=('arial', 12), bg='#ff7f7f')
+            valor.place(x=180, y=70)
+            valor1 = tk.Label(janela, text=lista_calculo[1], font=('arial', 12), bg='#ff7f7f')
+            valor1.place(x=180, y=100)
+            mais =  tk.Label(janela, text='+', font=('arial', 12), bg='#ff7f7f')
 botao = tk.Button(janela, text='salvar', font=('ArialBlack', 12), bg="#FFFFFF", command=salvar)
 botao.place(x=170 , y=120)
 
