8a35c6418b1c8fff1385fee0ef24c3c6cbe8b08b
Victor Hugo Ottati Reggiani <157662927+ottati1@users.noreply.github.com>
2025-10-13 08:05:33 -0300

    iniciei mais um exercicio

---
+++ feito com tkinter/ex04/ex4.py
@@ -0,0 +1,17 @@
+import tkinter as tk 
+
+janela = tk.Tk()
+janela.title('Exercício 04')
+janela.geometry('700x800')
+janela.resizable(False, False)
+
+janela.mainloop()
+# o programa tem que me dizer se oque foi digitado é 
+#variavel e qual variavel 
+#se só tem espaço
+#é um número
+#é uma letra 
+# e maiuscula ou minuscula 
+# a primeira letra e maiuscula ? 
+# faazer isso em loop e que cada vez que eu faça um novo atualize
+# se possivel criar um historico doque já foi pesquisado com uma haba onde eu possa acessar import tkinter as tk 
